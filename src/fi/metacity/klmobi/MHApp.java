93db81658353ffa9f90bdce871ecd6782904b3ef
Mikko Oksa <m3tacity@gmail.com>
2013-05-29 01:21:59 +0300

    Add route list and xmlResponse

--- src/fi/metacity/klmobi/MHApp.java
+++ src/fi/metacity/klmobi/MHApp.java
@@ -1,8 +1,11 @@
 package fi.metacity.klmobi;
 
-import com.googlecode.androidannotations.annotations.EApplication;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Application;
+
+import com.googlecode.androidannotations.annotations.EApplication;
 
 @EApplication
 public class MHApp extends Application {
@@ -10,6 +13,9 @@ public class MHApp extends Application {
 	private String mToken = "";
 	private Address mStartAddress;
 	private Address mEndAddress;
+	
+	private List<Route> mRoutes = new ArrayList<Route>();
+	private String mDetailsXmlRequest = "";
 	
 	public String getToken() {
 		return mToken;
@@ -33,5 +39,17 @@ public class MHApp extends Application {
 	
 	public void setEndAddress(Address address) {
 		mEndAddress = address;
+	}
+	
+	public List<Route> getRoutes() {
+		return mRoutes;
+	}
+	
+	public String getDetailsXmlRequest() {
+		return mDetailsXmlRequest;
+	}
+	
+	public void setDetailsXmlString(String xmlRequest) {
+		mDetailsXmlRequest = xmlRequest;
 	}
 }
