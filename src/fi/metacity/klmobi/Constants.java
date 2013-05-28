a47189a7cf1efdb2ba7bbc2d0c068f4dba762c85
Mikko Oksa <m3tacity@gmail.com>
2013-05-29 01:22:19 +0300

    Rename a var

--- src/fi/metacity/klmobi/Constants.java
+++ src/fi/metacity/klmobi/Constants.java
@@ -4,10 +4,11 @@ public final class Constants {
 	public static final String EXTRA_DATE = "fi.metacity.klmobi.extra.DATE";
 	public static final String EXTRA_TIME = "fi.metacity.klmobi.extra.TIME";
 	public static final String EXTRA_NUMBER_OF_ROUTES = "fi.metacity.klmobi.extra.NUMBER_OF_ROUTES";
-	public static final String EXTRA_ROUTE_TYPE = "fi.metacity.klmobi.extra.ROUTE_TYPE";
+	public static final String EXTRA_ROUTING_TYPE = "fi.metacity.klmobi.extra.ROUTING_TYPE";
 	public static final String EXTRA_WALKING_SPEED = "fi.metacity.klmobi.extra.WALKING_SPEED";
 	public static final String EXTRA_MAX_WALKING_DISTANCE = "fi.metacity.klmobi.extra.MAX_WALKING_DISTANCE";
 	public static final String EXTRA_CHANGE_MARGIN = "fi.metacity.klmobi.extra.CHANGE_MARGIN";
+	
 	public static final String EXTRA_ROUTE_INDEX = "fi.metacity.klmobi.extra.ROUTE_INDEX";
 	
 	public static final String[] citySubdomains = {
