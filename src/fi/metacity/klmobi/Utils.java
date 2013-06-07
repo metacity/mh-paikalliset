d63318e87df59fa1ba46cb6fb2ec3eefff9b4a9f
Mikko Oksa <m3tacity@gmail.com>
2013-06-07 21:36:42 +0300

    Encode params

--- src/fi/metacity/klmobi/Utils.java
+++ src/fi/metacity/klmobi/Utils.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
@@ -14,6 +15,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import android.net.Uri;
 import android.util.Log;
 
 public class Utils {
@@ -29,8 +31,8 @@ public class Utils {
 		Iterator<Entry<String, String>> iterator = params.entrySet().iterator();
 		while (iterator.hasNext()) {
 			Entry<String, String> param = iterator.next();
-			bodyBuilder.append(param.getKey()).append('=')
-				.append(URLEncoder.encode(param.getValue(), "UTF-8"));
+			bodyBuilder.append(Uri.encode(param.getKey())).append('=')
+				.append(URLEncoder.encode(param.getValue(), "UTF-8").replace("\\+", "%20"));
 			if (iterator.hasNext()) {
 				bodyBuilder.append('&');
 			}
@@ -42,27 +44,33 @@ public class Utils {
 		System.setProperty("http.keepAlive", "false");
 		try {
 			conn = (HttpURLConnection) url.openConnection();
+			conn.setDoInput(true);
 			conn.setDoOutput(true);
 			conn.setUseCaches(false);
 			conn.setFixedLengthStreamingMode(bytes.length);
 			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Connection", "close");
+			conn.setRequestProperty("Connection", "Close");
 			conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+			
+			conn.connect();
 			
 			// Post the request
 			OutputStream out = conn.getOutputStream();
 			out.write(bytes);
+			out.flush();
 			out.close();
-			
-			// Getting the InputStream has to happen before getting the status code
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					new BufferedInputStream(conn.getInputStream()), "UTF-8"), 8192);
 			
 			// Handle the response
 			int status = conn.getResponseCode();
 			if (status != 200) {
 				throw new IOException("POST failed with error code " + status);
 			}
+			
+			// Getting the InputStream has to happen before getting the status code
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					new BufferedInputStream(conn.getInputStream()), "UTF-8"), 8192);
+			
+			
 			
 			// Read the response
 			StringBuilder responseBuilder = new StringBuilder();
@@ -90,6 +98,7 @@ public class Utils {
 		try {
 			conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
+			conn.setDoInput(true);
 			conn.setRequestProperty("Connection", "close");
 			conn.setRequestProperty("Content-Type", "text/html;charset=UTF-8");
 			
