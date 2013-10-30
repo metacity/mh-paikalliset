549e7ccf67ca94a2039ac8c8dc72b2622d521e04
Mikko Oksa <m3tacity@gmail.com>
2013-10-31 00:42:09 +0200

    Remove commented line

--- src/fi/metacity/klmobi/RouteResultsFragment.java
+++ src/fi/metacity/klmobi/RouteResultsFragment.java
@@ -113,7 +113,7 @@ public class RouteResultsFragment extends ListFragment {
 		listView.addHeaderView(header);
 		listView.setBackgroundResource(R.color.background);
 		listView.setDivider(getResources().getDrawable(android.R.color.transparent));
-		listView.setDividerHeight(Utils.dpsToPixels(getActivity(), 10));
+		//listView.setDividerHeight(Utils.dpsToPixels(getActivity(), 10));
 
 		if (mGlobals.getRoutes().isEmpty()) {
 			fetchRoutes();
